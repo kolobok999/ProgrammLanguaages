4da63c3dfae80b9d347e3b58ab72f21673d89c00
Egor Dechenko <gog.992@gmail.com>
2023-02-24 10:52:52 +0300

    Task_2

---
+++ Lesson_5/Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу замена элементов массива:
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+using static Task_2.Library;
+
+int[] ChangeElements(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = arr[i] * (-1);
+    }
+    return newArr;
+}
+
+int[] array = new int[10];
+int[] newArray = new int[10];
+
+WriteArray(array, -100, 100);
+
+
+newArray = ChangeElements(array);
+System.Console.WriteLine($"[ {PrintArray(array)}] -> [ {PrintArray(newArray)}]");
