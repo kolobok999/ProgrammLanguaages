88155105933c2c0ce9084fe56f73175d0cf3d4ce
Egor Dechenko <gog.992@gmail.com>
2023-02-24 12:11:36 +0300

    Task_3

--- Lesson_5/Task_3/Library.cs
+++ Lesson_5/Task_3/Library.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Task_2
+namespace Task_3
 {
     public class Library
     {
--- Lesson_5/Task_3/Program.cs
+++ Lesson_5/Task_3/Program.cs
@@ -1,3 +1,34 @@
 ﻿// Задача 3: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // -3; массив [6, 7, 19, 345, 3] -> да
+
+using static Task_3.Library;
+
+bool FindNumber(int[] arr, int n)
+{
+    bool isFindNumber = false;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == n)
+        {
+            isFindNumber = true;
+            break;
+        }
+    }
+    return isFindNumber;
+}
+
+void PrintResult(int n, int[] arr, string str)
+{
+    System.Console.WriteLine($"{n}; массив [ {PrintArray(arr)}] -> {str}");
+}
+
+int[] array = new int[20];
+WriteArray(array, 1, 20);
+
+int number = ReadNumber();
+
+if (FindNumber(array, number))
+    PrintResult(number, array, "да");
+else
+    PrintResult(number, array, "нет");
