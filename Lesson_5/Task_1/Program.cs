a1ae3464a0ec3fc67834a055335cf13a406359f5
Egor Dechenko <gog.992@gmail.com>
2023-02-24 10:12:08 +0300

    Task_1

--- Lesson_5/Task_1/Program.cs
+++ Lesson_5/Task_1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9].
+// Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+using static Task_1.Library;
+
+int FindPositiveSum(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+int FindNegativeSum(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < 0)
+        {
+            result += arr[i];
+        }
+    }
+    return result;
+}
+
+int[] array = new int[12];
+WriteArray(array, -9, 10);
+
+
+System.Console.WriteLine($" [{PrintArray(array)}] сумма положительных чисел равна {FindPositiveSum(array)}, сумма отрицательных равна - {FindNegativeSum(array)}");
+
