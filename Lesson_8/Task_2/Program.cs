50424b86cef47927639a29a312d9afc094a59943
Egor Dechenko <gog.992@gmail.com>
2023-03-07 11:35:02 +0300

    Task_2

--- Lesson_8/Task_2/Program.cs
+++ Lesson_8/Task_2/Program.cs
@@ -10,18 +10,13 @@ int[,] SwapStrings(int[,] matrix)
         int[] pair = { 0, j };
         SwapMatrix(matrix, new int[] { 0, j }, new int[] { row - 1, j });
     }
-
     return matrix;
 }
 
-
 int[,] matrix = new int[4, 3];
-
 matrix = FillMatrix(matrix, 0, 20);
 PrintMatrix(matrix);
 
 matrix = SwapStrings(matrix);
 System.Console.WriteLine();
 PrintMatrix(matrix);
-
-// https://us04web.zoom.us/j/77453872895?pwd=9mYqdZhom083KnaDbYqBAQYHXVsMq1.1
